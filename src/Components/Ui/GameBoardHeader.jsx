3a9dc8ac18dfd410df0e1df29cf93e65d640c368
abdul qadoos <abdul.qadoos@n0name.eu>
2022-06-20 12:41:25 +0500

    Game Board UI

--- src/Components/Ui/GameBoardHeader.jsx
+++ src/Components/Ui/GameBoardHeader.jsx
@@ -4,10 +4,10 @@ export default function GameBoardHeader() {
     return (
         <div className="bg-secondary pb-2 lg:p-3 text-white">
             <div className="container mx-auto py-5">
-
+                <div className=""></div>
                 <div className="grid grid-cols-4 gap-5 items-end">
 
-                    <div className="text-center ">
+                    <div className="text-center">
                         <h1 className="md:text-3xl font-default font-light">TEAM 1 NAME <span
                             className="text-xl">8-8</span></h1>
                         <div className="w-20 h-20 md:w-40 md:h-40 bg-white rounded-full my-5 overflow-hidden mx-auto ">
@@ -26,11 +26,42 @@ export default function GameBoardHeader() {
                     </div>
 
                     <div className="font-default col-span-2">
-                        <div className="flex justify-around items-center">
+                        <div className="flex justify-around items-center mb-16">
                             <h1 className="text-5xl">45</h1>
                             <h1 className=" flex flex-col text-center text-4xl"><span>6:02</span> <span
                                 className="text-xl ">4th</span></h1>
                             <h1 className="text-5xl">52</h1>
+                        </div>
+                        <div className="w-full">
+                            <div className="grid grid-cols-9 text-center font-bold">
+                                <span className="bg-secondary-light h-6 col-span-3"></span>
+                                <span className="bg-secondary-light h-6">1</span>
+                                <span className="bg-secondary-light h-6">2</span>
+                                <span className="bg-secondary-light h-6">3</span>
+                                <span className="bg-secondary-light h-6">4</span>
+                                <span className="bg-secondary-light h-6">OT</span>
+                                <span className="bg-secondary-light h-6"></span>
+                            </div>
+
+                            <div className="grid grid-cols-9 gap-0.5 text-secondary">
+                                <span className="bg-white h-9 leading-9 px-2 col-span-3">Team 1</span>
+                                <span className="bg-white h-9 leading-9 text-center">1</span>
+                                <span className="bg-white h-9 leading-9 text-center">2</span>
+                                <span className="bg-white h-9 leading-9 text-center">3</span>
+                                <span className="bg-white h-9 leading-9 text-center">4</span>
+                                <span className="bg-white h-9 leading-9 text-center">OT</span>
+                                <span className="bg-white h-9 leading-9 text-center"></span>
+                            </div>
+
+                            <div className="grid grid-cols-9 gap-0.5 mt-0.5 text-secondary">
+                                <span className="bg-white h-9 leading-9 px-2 col-span-3">Team 2</span>
+                                <span className="bg-white h-9 leading-9 text-center">1</span>
+                                <span className="bg-white h-9 leading-9 text-center">2</span>
+                                <span className="bg-white h-9 leading-9 text-center">3</span>
+                                <span className="bg-white h-9 leading-9 text-center">4</span>
+                                <span className="bg-white h-9 leading-9 text-center">OT</span>
+                                <span className="bg-white h-9 leading-9 text-center"></span>
+                            </div>
                         </div>
                     </div>
 

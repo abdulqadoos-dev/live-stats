389e0ebec03b8488dda78477380ecee52513c4b2
abdul qadoos <abdulqadoos.work@gmail.com>
2022-08-24 12:09:01 +0500

    responsive issue resolved

--- src/Components/Ui/GameBoardHeader.jsx
+++ src/Components/Ui/GameBoardHeader.jsx
@@ -7,7 +7,7 @@ import Timer from "./Timer";
 export default function GameBoardHeader({match, changeMatchState, numberOfHalf, calculateTeamHalf}) {
     const {user} = JSON.parse(window.localStorage.getItem(LOCAL_STORAGE_AUTH_USER));
     return (
-        <div className="bg-secondary  text-white">
+        <div className="bg-secondary text-white">
             {user.roleId === TEAM_ROLE_ID ? (
                 <div className="container mx-auto">
                     <div className="flex justify-between game-board-actions">
@@ -33,19 +33,19 @@ export default function GameBoardHeader({match, changeMatchState, numberOfHalf, 
                 </div>
             ) : null}
 
-            <div className="container mx-auto pb-5 pt-2">
-                <div className="grid grid-cols-4 gap-5 items-end">
-                    <div className="lg:text-center col-span-1">
+            <div className="container mx-auto pb-5 p-2">
 
-                        <h1 className="md:text-2xl font-default font-light">
-                            {match?.game?.mainTeam?.name || ""} <span className="text-sm">8-8</span>
+                <div className="grid grid-cols-4 gap-1 xl:gap-5">
+
+                    {/* main team */}
+                    <div className="text-center col-span-2 lg:col-span-1">
+                        <h1 className="font-default font-light lg:text-xl">
+                            {match?.game?.mainTeam?.name || ""} <span className="text-sm hidden sm:inline-block">8-8</span>
                         </h1>
-
-                        <div className="w-24 h-24 bg-white rounded-full my-2 overflow-hidden lg:mx-auto ">
-                            <img src={process.env.REACT_APP_SERVER_PATH + "/uploadedImage"} className="w-56 lg:w-64"/>
+                        <div className="w-16 h-16 lg:w-24 lg:h-24 bg-white rounded-full my-2 overflow-hidden mx-auto ">
+                            <img src={process.env.REACT_APP_SERVER_PATH + "/image.jpg"} />
                         </div>
-
-                        <div className="my-3 gap-3 flex lg:justify-center">
+                        <div className="my-1 lg:my-3 gap-3 flex justify-center">
                             <span
                                 className={`p-1.5  ${match?.matchDetails.mainTeam.find(timeout => timeout.half === match.matchDetails.activeHalf).timeouts >= 1 ? "bg-yellow-300" : "bg-light"}  rounded-full`}/>
                             <span
@@ -57,15 +57,14 @@ export default function GameBoardHeader({match, changeMatchState, numberOfHalf, 
                             <span
                                 className={`p-1.5  ${match?.matchDetails.mainTeam.find(timeout => timeout.half === match.matchDetails.activeHalf).timeouts >= 5 ? "bg-yellow-300" : "bg-light"}  rounded-full`}/>
                         </div>
-
                         <div className="font-default">
                             {match?.matchDetails.mainTeam.find(timeout => timeout.half === match.matchDetails.activeHalf).bonusPlus ? "Bonus +" : match?.matchDetails.mainTeam.find(timeout => timeout.half === match.matchDetails.activeHalf).bonus && "Bounce"}
                         </div>
-
                     </div>
 
-                    <div className="font-default col-span-2">
-                        <div className="flex justify-around items-center mb-5">
+                    {/* score board */}
+                    <div className="font-default col-span-4  row-start-2 lg:row-auto lg:col-span-2  ">
+                        <div className="flex justify-around items-center mb-3 lg:mb-5">
                             <h1 className="text-3xl">{match?.matchPlayers.mainTeamTotal || 0}</h1>
                             <h1 className="flex flex-col text-center text-3xl">
                                 {match?.matchDetails?.activeHalfTime ? (
@@ -111,13 +110,16 @@ export default function GameBoardHeader({match, changeMatchState, numberOfHalf, 
                         </div>
                     </div>
 
-                    <div className="text-center">
-                        <h1 className="md:text-2xl font-default font-light"><span
-                            className="text-sm">12-8</span> {match?.game?.opponentTeam?.name || ""} </h1>
-                        <div className="w-24 h-24  bg-white rounded-full my-2 overflow-hidden mx-auto ">
-                            <img src={process.env.REACT_APP_SERVER_PATH + "/uploadedImage"} className="w-56 lg:w-64"/>
+                    <div className="text-center col-span-2 lg:col-span-1">
+
+                        <h1 className="font-default font-light lg:text-xl">
+                            <span className="text-sm hidden sm:inline-block">12-8</span> {match?.game?.opponentTeam?.name || ""}
+                        </h1>
+
+                        <div className="w-16 h-16 lg:w-24 lg:h-24 bg-white rounded-full my-2 overflow-hidden mx-auto ">
+                            <img src={process.env.REACT_APP_SERVER_PATH + "/image.jpg"} />
                         </div>
-                        <div className="my-3 gap-3 flex justify-center">
+                        <div className="my-1 lg:my-3 gap-3 flex justify-center">
                             <span
                                 className={`p-1.5  ${match?.matchDetails.opponentTeam.find(timeout => timeout.half === match.matchDetails.activeHalf).timeouts >= 1 ? "bg-yellow-300" : "bg-light"}  rounded-full`}/>
                             <span
